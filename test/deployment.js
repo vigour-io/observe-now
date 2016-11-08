eab36a87aa354ea22cb48ec99f79e601bc0c6eeb
Mustafa Dokumaci <mustafa@foraa.com>
2016-11-08 17:00:40 +0100

    test deployment

---
+++ test/deployment.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const test = require('tape')
+const sinon = require('sinon')
+const Now = require('now').default
+const Alias = require('now/build/lib/alias').default
+
+const now = require('../lib')
+
+process.env.NOW_TOKEN = 'NOW-TOKEN'
+
+test('deploy - alias', t => {
+  const create = sinon.stub(Now.prototype, 'create')
+  const set = sinon.stub(Alias.prototype, 'set')
+  const get = sinon.stub(now, 'get')
+
+  Now.prototype._id = 'deployment-id'
+  Now.prototype._host = 'deployment-url.now.sh'
+  Now.prototype._syncAmount = 0
+  Now.prototype._missing = []
+
+  create
+    .withArgs('directory', {
+      env: {
+        NODE_ENV: 'production',
+        env1: 'val1'
+      },
+      forwardNpm: true,
+      quiet: true
+    })
+    .returns(Promise.resolve())
+
+  get
+    .withArgs('deployments/deployment-id', 'NOW-TOKEN', false)
+    .returns({
+      on (e, cb) {
+        if (e === 'data') {
+          setTimeout(cb, 0, { state: 'READY' })
+        }
+        return this
+      },
+      send () {}
+    })
+
+  set
+    .withArgs('deployment-id', 'some-domain.com')
+    .returns(Promise.resolve())
+
+  t.plan(4)
+
+  const deployment = now.deploy(process.env.NOW_TOKEN)
+
+  deployment
+    .deploy('directory', {env1: 'val1'})
+    .on('deployed', () => {
+      t.equals(deployment.id.compute(), 'deployment-id', 'has deployment id')
+      t.equals(deployment.url.compute(), 'https://deployment-url.now.sh', 'has deployment url')
+    })
+    .on('ready', () => {
+      t.ok(true, 'ready')
+      deployment.alias('some-domain.com')
+    })
+    .on('aliased', () => {
+      t.ok(true, 'aliased')
+      deployment.remove()
+      create.restore()
+      set.restore()
+      get.restore()
+    })
+    .on('error', error => {
+      console.error('Deployment failed due to error: %j, stack: %s', error, error ? error.stack : '(no stack)')
+    })
+})
+
+test('load - alias', t => {
+  const findDeployment = sinon.stub(Alias.prototype, 'findDeployment')
+  const set = sinon.stub(Alias.prototype, 'set')
+
+  findDeployment
+    .withArgs('https://deployment-url.now.sh')
+    .returns(Promise.resolve({ uid: 'deployment-id', url: 'https://deployment-url.now.sh' }))
+
+  set
+    .withArgs('deployment-id', 'some-domain.com')
+    .returns(Promise.resolve())
+
+  t.plan(2)
+
+  const deployment = now.deploy(process.env.NOW_TOKEN)
+
+  deployment
+    .load('https://deployment-url.now.sh')
+    .on('loaded', () => {
+      t.ok(true, 'loaded')
+      deployment.alias('some-domain.com')
+    })
+    .on('aliased', () => {
+      t.ok(true, 'aliased')
+      deployment.remove()
+    })
+    .on('error', error => {
+      console.error('Alias failed due to error: %j, stack: %s', error, error ? error.stack : '(no stack)')
+    })
+})
